acd8f87972aa7d764a209b1f9568c0d45a25ccea
Adams <ja10amt@bolton.ac.uk>
2017-02-22 09:09:27 +0000

    AAAAAAAAAHHHHHH SOURCE TREE

--- Assets/Scripts_2/Components/Damage/Destruction/destruction_object_component.cs
+++ Assets/Scripts_2/Components/Damage/Destruction/destruction_object_component.cs
@@ -5,7 +5,7 @@ public class destruction_object_component : destruction_component {
 
     [SerializeField]
     private GameObject replacement_object;
-
+    public GameObject chicken_object;
     public float explosive_force;
     public float explosion_radius;
 
@@ -20,16 +20,25 @@ public class destruction_object_component : destruction_component {
 
     public override void On_Health_Is_Zero()
     {
-        if(particle_emitter_prefab != null)
+        float chicken_chance = Random.Range(0, 100);
+        if (chicken_chance < dev_input.developer_codes.chicken_chance && chicken_object != null)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject chicken = Instantiate(chicken_object, this.transform.position, Quaternion.identity) as GameObject;
+                chicken.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)) * 5, ForceMode.Impulse);
+            }
+        }
+        if (particle_emitter_prefab != null)
         {
             GameObject obj = Instantiate(particle_emitter_prefab, this.transform.position, Quaternion.identity) as GameObject;
             obj.transform.Rotate(new Vector3(90, 0, 0));
         }
-        if(replacement_object != null)
+        if (replacement_object != null)
         {
             Rigidbody object_rigidbody = GetComponent<Rigidbody>();
             GameObject destroyed = Instantiate(replacement_object, this.transform.position, this.transform.rotation) as GameObject;
-            for(int i = 0; i < destroyed.transform.childCount; i++)
+            for (int i = 0; i < destroyed.transform.childCount; i++)
             {
                 Rigidbody rbody = destroyed.transform.GetChild(i).gameObject.GetComponentInChildren<Rigidbody>();
                 if (null != rbody && object_rigidbody != null)
@@ -39,15 +48,16 @@ public class destruction_object_component : destruction_component {
             }
             rage_component.global_rage_component.total_rage += 10;
         }
-        if(score_component == null)
+        if (score_component == null)
         {
             score_component = FindObjectOfType<character_score_component>();
         }
 
-        if(score_component != null)
+        if (score_component != null)
         {
             score_component.Modify_Score(10);
         }
+        
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts_2/Dev/dev_input.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dev_input : MonoBehaviour {
+
+    public static dev_input developer_codes;
+
+    string[] input_codes = { "kfc", "rageon" };
+    string input;
+
+    public int chicken_chance = 9;
+
+	// Use this for initialization
+	void Start () {
+	    if(developer_codes == null)
+        {
+            developer_codes = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Handle_Input();
+	}
+
+    void Handle_Input()
+    {
+        foreach(char c in Input.inputString)
+        {
+            if(c >= 'a' && c <= 'z')
+            {
+                input += c;
+                Code_Check();
+            }
+        }
+    }
+
+    void Code_Check()
+    {
+        for(int i = 0; i < input_codes.Length; i++)
+        {
+            if (input.Contains(input_codes[i]))
+            {
+                switch(input_codes[i])
+                {
+                    case "kfc":
+                        Chicken_Time();
+                        break;
+                    case "rageon":
+                        Rage_On();
+                        break;
+                    default:
+                        break;
+                }
+                input = "";
+            }
+        }
+    }
+
+    void Chicken_Time()
+    {
+        chicken_chance = 100;
+    }
+
+    void Rage_On()
+    {
+        if (rage_component.global_rage_component != null)
+        {
+            rage_component.global_rage_component.current_rage = 1000;
+        }
+    }
+}
